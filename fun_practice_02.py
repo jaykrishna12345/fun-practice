92502c62ab8207f496ab0e4773eb1a86c9bdd242
jay krishna das <108941785+jaykrishna12345@users.noreply.github.com>
2023-07-15 18:12:28 +0530

    Add files via upload

---
+++ fun_practice_02.py
@@ -0,0 +1,35 @@
+'''list_1=[34,54,72,90,10,18,63,75,36,54]
+number=int(input('enter a number from above list to check if it is divisible by 9 :'))
+
+
+if number not in list_1 :
+    print(' the number is not listed in the list but')
+if number%9==0 :
+    print('the number is divisible by 9')
+else :
+    print('the number is not divisible by 9')'''
+
+
+
+
+
+
+'''print('enter some numbers to add them in a list')
+num_1=int(input('enter the first number:' ))
+num_2= int(input('enter the second number:' ))
+num_3=int(input('enter the third number:' ))
+num_4=int(input('enter the fourth number:' ))
+num_5=int(input('enter the fifth number:' ))
+
+num_list=[num_1,num_2,num_3,num_4,num_5]
+print('the list of numbers are:',num_list)
+num=int(input('enter any number from above list to \n check if the number is divisible by 9 :'))
+if num not in num_list:
+    print('the number is not listed in the list, but')
+if num%9==0 :
+ print('the number is divisible by 9')
+else :
+   print('the number is not divisible by 9')'''
+
+
+
